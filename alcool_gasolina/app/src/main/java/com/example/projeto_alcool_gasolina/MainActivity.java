ee6d36452ada864c22a4bc211b9036572543414c
gustavojullio <gustavo-julio@live.com>
2024-10-23 10:20:40 -0300

    Update

---
+++ alcool_gasolina/app/src/main/java/com/example/projeto_alcool_gasolina/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.projeto_alcool_gasolina;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn = findViewById(R.id.button);
+        EditText gasolina = findViewById(R.id.editTextText);
+        EditText alcool = findViewById(R.id.editTextText2);
+        TextView resultado = findViewById(R.id.textView2);
+
+        gasolina.addTextChangedListener(new MoneyTextWatcher(gasolina));
+        alcool.addTextChangedListener(new MoneyTextWatcher(alcool));
+
+
+
+    }
+    public void calc(View view){
+
+    }
+}
---
+++ alcool_gasolina/app/src/main/java/com/example/projeto_alcool_gasolina/MoneyTextWatcher.java
@@ -0,0 +1,44 @@
+package com.example.projeto_alcool_gasolina;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class MoneyTextWatcher implements TextWatcher {
+
+    private final EditText editText;
+
+    public MoneyTextWatcher(EditText editText) {
+        this.editText = editText;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        if (!charSequence.equals("")) {
+            editText.removeTextChangedListener(this);
+
+            String clearString = charSequence.toString().replaceAll("[^\\d]", "");
+
+            double gasolinaConvertida = Double.parseDouble(clearString);
+
+            String formatted = NumberFormat.getCurrencyInstance(new Locale("pt","BR")).format(gasolinaConvertida / 100);
+            editText.setText(formatted);
+            editText.setSelection(formatted.length());
+
+            editText.addTextChangedListener(this);
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+}
