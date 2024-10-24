a309e07e0eba34e36d4371faae184314aa737e32
Gustavo Júlio <gustavo-julio@live.com>
2024-10-23 21:38:07 -0300

    Alterações no exercício.

--- alcool_gasolina/app/src/main/java/com/example/projeto_alcool_gasolina/MainActivity.java
+++ alcool_gasolina/app/src/main/java/com/example/projeto_alcool_gasolina/MainActivity.java
@@ -6,6 +6,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,10 +40,40 @@ public class MainActivity extends AppCompatActivity {
         gasolina.addTextChangedListener(new MoneyTextWatcher(gasolina));
         alcool.addTextChangedListener(new MoneyTextWatcher(alcool));
 
-
-
     }
     public void calc(View view){
+        EditText gasolina = findViewById(R.id.editTextText);
+        EditText alcool = findViewById(R.id.editTextText2);
+        TextView resultado = findViewById(R.id.textView2);
+        ImageView img = findViewById(R.id.imageView);
+
+        if (!gasolina.getText().toString().equals("") && !alcool.getText().toString().equals("")){
+            try {
+                String clearStringGasolina = gasolina.getText().toString().replaceAll("[^\\d]", "");
+                String clearStringAlcool = alcool.getText().toString().replaceAll("[^\\d]", "");
+
+                double valorGasolina = Double.parseDouble(clearStringGasolina);
+                double valorAlcool = Double.parseDouble(clearStringAlcool);
+
+                if (valorAlcool / valorGasolina < 0.7){
+                    resultado.setText("Resultado: Abasteça com Álcool.");
+                    img.setImageResource(R.drawable.alcool);
+                }else{
+                    resultado.setText("Resultado: Abasteça com Gasolina.");
+                    img.setImageResource(R.drawable.gasolina);
+                }
+
+            }catch (Exception e){
+                Toast.makeText(this, "Erro ao capturar os valores", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "Preencha todos os campos. ", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+
 
     }
 }
