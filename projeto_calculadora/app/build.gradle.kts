a6f437582f57ff286ae9f7d3939cee371d023bf0
gustavojullio <gustavo-julio@live.com>
2024-11-27 10:04:46 -0300

    Atualização dos testes.

--- projeto_calculadora/app/build.gradle.kts
+++ projeto_calculadora/app/build.gradle.kts
@@ -1,62 +1,3 @@
-//plugins {
-//    alias(libs.plugins.android.application)
-//}
-//
-//android {
-//    namespace = "com.example.projeto_calculadora"
-//    compileSdk = 34
-//
-//    testOptions{
-//        unitTests.isIncludeAndroidResources = true
-//    }
-//    defaultConfig {
-//        applicationId = "com.example.projeto_calculadora"
-//        minSdk = 24
-//        targetSdk = 34
-//        versionCode = 1
-//        versionName = "1.0"
-//
-//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//    }
-//
-//    buildTypes {
-//        release {
-//            isMinifyEnabled = false
-//            proguardFiles(
-//                getDefaultProguardFile("proguard-android-optimize.txt"),
-//                "proguard-rules.pro"
-//            )
-//        }
-//    }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_1_8
-//        targetCompatibility = JavaVersion.VERSION_1_8
-//    }
-//}
-//
-//dependencies {
-//
-//    implementation(libs.appcompat)
-//    implementation(libs.material)
-//    implementation(libs.activity)
-//    implementation(libs.constraintlayout)
-//
-//    testImplementation(libs.junit)
-//    //testImplementation(libs.mockito.core)
-//    androidTestImplementation(libs.ext.junit)
-//
-//
-//    androidTestImplementation(libs.ext.junit)
-//    androidTestImplementation(libs.espresso.core)
-//    androidTestImplementation("androidx.test.espresso:espresso-idling-resource:3.5.1") // Adicionando a versão corretamente
-//
-//    //androidTestImplementation(libs.mockito.android)
-//
-//    androidTestImplementation(libs.test.rules)
-//
-//
-//}
-
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -96,17 +37,17 @@ android {
 }
 
 dependencies {
-    // Bibliotecas principais
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
 
-    // Testes unitários
+
     testImplementation(libs.junit)
     testImplementation(libs.mockito.core)
 
-    // Testes instrumentados
+
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.espresso.idling.resource)
--- projeto_calculadora/app/src/androidTest/java/com/example/projeto_calculadora/MainActivityTest.java
+++ projeto_calculadora/app/src/androidTest/java/com/example/projeto_calculadora/MainActivityTest.java
@@ -37,7 +37,7 @@ public class MainActivityTest {
         onView(withId(R.id.tres)).perform(click()); // Press "3"
 
         // Verify input TextView shows "8+3"
-        onView(withId(R.id.operacao)).check(matches(withText("8+3")));
+        onView(withId(R.id.operacao)).check(matches(withText("8 + 3")));
     }
 
     @Test
@@ -51,5 +51,101 @@ public class MainActivityTest {
         // Verify output TextView shows the result "11"
         onView(withId(R.id.resultado)).check(matches(withText("11.0")));
     }
+
+    @Test
+    public void testeSaidaSubtracao(){
+        // Simulação quando pressiona no botão de subtração
+        onView(withId(R.id.oito)).perform(click()); // Pressiona "8"
+        onView(withId(R.id.subtracao)).perform(click()); // Pressiona "-"
+        onView(withId(R.id.tres)).perform(click()); // Pressiona "3"
+        onView(withId(R.id.igual)).perform(click()); // Pressiona "=" para calcular
+
+        // Verifica se a saída do TextView foi igual a "5"
+        onView(withId(R.id.resultado)).check(matches(withText("5.0")));
+    }
+
+    @Test
+    public void testeSaidaMultiplicacao(){
+        // Simulação quando pressiona no botão de multiplicação
+        onView(withId(R.id.oito)).perform(click()); // Pressiona "8"
+        onView(withId(R.id.multiplicacao)).perform(click()); // Pressiona "x"
+        onView(withId(R.id.tres)).perform(click()); // Pressiona "3"
+        onView(withId(R.id.igual)).perform(click()); // Pressiona "=" para calcular
+
+        // Verifica se a saída do TextView foi igual a "24"
+        onView(withId(R.id.resultado)).check(matches(withText("24.0")));
+    }
+
+    @Test
+    public void testeSaidaDivisao(){
+        // Simulação quando pressiona no botão de divisão
+        onView(withId(R.id.nove)).perform(click()); // Pressiona "9"
+        onView(withId(R.id.divisao)).perform(click()); // Pressiona "/"
+        onView(withId(R.id.tres)).perform(click()); // Pressiona "3"
+        onView(withId(R.id.igual)).perform(click()); // Pressiona "=" para calcular
+
+        // Verifica se a saída do TextView foi igual a "3"
+        onView(withId(R.id.resultado)).check(matches(withText("3.0")));
+    }
+
+    @Test
+    public void testeSaidaDivisaoPorZero(){
+        // Simulação quando pressiona no botão de divisao por zero
+        onView(withId(R.id.nove)).perform(click()); // Pressiona "9"
+        onView(withId(R.id.divisao)).perform(click()); // Pressiona "/"
+        onView(withId(R.id.zero)).perform(click()); // Pressiona "0"
+        onView(withId(R.id.igual)).perform(click()); // Pressiona "=" para calcular
+
+        // Verifica se a saída do TextView foi igual a "Não é possível dividir por zero!"
+        onView(withId(R.id.resultado)).check(matches(withText("Não é possível dividir por zero!")));
+    }
+
+    @Test
+    public void testePorcentagem(){
+        // Simulação quando pressiona no botão de porcentagem
+        onView(withId(R.id.nove)).perform(click()); // Pressiona "9"
+        onView(withId(R.id.porcentagem)).perform(click()); // Pressiona "%"
+
+        // Verifica se a saída do TextView foi igual a "0.09"
+        onView(withId(R.id.resultado)).check(matches(withText("0.09")));
+    }
+
+    @Test
+    public void testeInverterSinal(){
+        // Simulação quando pressiona no botão de +/-
+        onView(withId(R.id.nove)).perform(click()); // Pressiona "9"
+        onView(withId(R.id.maisMenos)).perform(click()); // Pressiona "+/-"
+
+        // Verifica se a saída do TextView foi igual a "-9"
+        onView(withId(R.id.operacao)).check(matches(withText("-9.0")));
+    }
+
+    @Test
+    public void testeAc(){
+        // Simulação quando pressiona no botão de AC
+        onView(withId(R.id.nove)).perform(click()); // Pressiona "9"
+        onView(withId(R.id.multiplicacao)).perform(click()); // Pressiona "x"
+        onView(withId(R.id.tres)).perform(click()); // Pressiona "3"
+        onView(withId(R.id.igual)).perform(click()); // Pressiona "=" para calcular
+        onView(withId(R.id.ac)).perform(click()); // Pressiona "AC" para limpar
+
+        // Verifica se a saída do TextView foi igual a ""
+        onView(withId(R.id.operacao)).check(matches(withText("")));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
