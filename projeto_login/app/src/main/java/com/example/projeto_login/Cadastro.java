a20d620da0d92028bf60987d60661481af361581
gustavojullio <gustavo-julio@live.com>
2025-02-13 09:52:24 -0300

    Atualização do projeto de Login.

--- projeto_login/app/src/main/java/com/example/projeto_login/Cadastro.java
+++ projeto_login/app/src/main/java/com/example/projeto_login/Cadastro.java
@@ -1,15 +1,11 @@
 package com.example.projeto_login;
-
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
--- projeto_login/app/src/main/java/com/example/projeto_login/MainActivity.java
+++ projeto_login/app/src/main/java/com/example/projeto_login/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.projeto_login;
-
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
